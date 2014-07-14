0feea3db1e3bb9b78811e38d71ceaa0d2cffae4d
Duane Bekaert <duane.bekaert@gmail.com>
2014-07-14 20:33:23 +0200

    Added further dcumentation about the option negociation

--- include/constants.h
+++ include/constants.h
@@ -2,14 +2,88 @@
 #define __CONSTANTS_H__
 
 // Buffer length for the exchanged messages
-#define BUFLEN 20
+#define BUFLEN      20
 
-// Command code for the dialog with the server
-#define DO 0xfd
-#define WONT 0xfc
-#define WILL 0xfb
-#define DONT 0xfe
-#define CMD 0xff
+// Command code for the option negociation the server
+#define DO          0xfd
+#define WONT        0xfc
+#define WILL        0xfb
+#define DONT        0xfe
+#define CMD         0xff
+
+#define SE          240 // End of subnegotiation parameters.
+#define NOP         241 // No operation
+#define DATA_MARK   242
+#define BRK         234 // Break
+#define IP          244 // Interupt process
+#define AO          245 // Abort output
+#define AYT         246 // Are you there ?
+#define EC          247 // Erase character
+#define EL          248 // Erase line
+#define GA          249 // Go ahead
+#define SB          250 // What follows is a subnegciation parameter
+#define CWILL       251
+#define CWONT       252
+#define CDO         253
+#define CDONT       254
+#define IAC         255 // Beggiing of a command
+
+/*
+0 	Binary Transmission
+1 	Echo
+2 	Reconnection 
+3 	Suppress Go Ahead 
+4 	Approx Message Size Negotiation
+5 	Status 
+6 	Timing Mark 
+7 	Remote Controlled Trans and Echo
+8 	Output Line Width 
+9 	Output Page Size
+10 	Output Carriage-Return Disposition 
+11 	Output Horizontal Tab Stops
+12 	Output Horizontal Tab Disposition 
+13 	Output Formfeed Disposition 
+14 	Output Vertical Tabstops 
+15 	Output Vertical Tab Disposition
+16 	Output Linefeed Disposition 
+17 	Extended ASCII 
+18 	Logout 
+19 	Byte Macro 
+20 	Data Entry Terminal
+21 	SUPDUP 
+22 	SUPDUP Output 
+23 	Send Location 
+24 	Terminal Type 
+25 	End of Record 
+26 	TACACS User Identification 
+27 	Output Marking 
+28 	Terminal Location Number 
+29 	Telnet 3270 Regime 
+30 	X.3 PAD 
+31 	Negotiate About Window Size 
+32 	Terminal Speed 
+33 	Remote Flow Control 
+34 	Linemode 
+35 	X Display Location 
+36 	Environment Option 
+37 	Authentication Option 
+38 	Encryption Option
+39 	New Environment Option 
+40 	TN3270E 
+41 	XAUTH 
+42 	CHARSET 
+43 	Telnet Remote Serial Port (RSP) 
+44 	Com Port Control Option 
+45 	Telnet Suppress Local Echo 
+46 	Telnet Start TLS 
+47 	KERMIT 	
+48 	SEND-URL 
+49 	FORWARD_X
+138 	TELOPT PRAGMA LOGON
+139 	TELOPT SSPI LOGON 
+140 	TELOPT PRAGMA HEARTBEAT 
+255 	Extended-Options-List
+*/
 
 #define CMD_ECHO 1
 #define CMD_WINDOW_SIZE 31
--- src/tnc.c
+++ src/tnc.c
@@ -8,11 +8,13 @@ void negotiate(int sock, unsigned char *buf, int len) {
     int i;
      
     if (buf[1] == DO && buf[2] == CMD_WINDOW_SIZE) {
-        unsigned char tmp1[10] = {255, 251, 31};
+        // I want to start a negociation about the window size
+        unsigned char tmp1[10] = {IAC, CWILL, 31};
         if (send(sock, tmp1, 3 , 0) < 0)
             exit(1);
          
-        unsigned char tmp2[10] = {255, 250, 31, 0, 80, 0, 24, 255, 240};
+        // I want a windows that is 80 columns wide and 24 lines long
+        unsigned char tmp2[10] = {IAC, SB, 31, 0, 80, 0, 24, IAC, SE};
         if (send(sock, tmp2, 9, 0) < 0)
             exit(1);
         return;
@@ -49,12 +51,11 @@ tnc_error tnc (char * host, char * port, char * user, unsigned int login) {
     }
     puts("Connected...\n");
  
-    // set terminal
     terminal_set();
     atexit(terminal_reset);
 
     struct timeval ts;
-    ts.tv_sec = 1; // 1 second
+    ts.tv_sec = 1;
     ts.tv_usec = 0;
  
     while (1) {
@@ -70,7 +71,7 @@ tnc_error tnc (char * host, char * port, char * user, unsigned int login) {
         if (nready < 0) {
             return TNCE_ERROR;
         } else if (nready == 0) {
-            ts.tv_sec = 1; // second
+            ts.tv_sec = 1;
             ts.tv_usec = 0;
         } else if (sock != 0 && FD_ISSET(sock, &fds)) {
             // start by reading a single byte
@@ -89,6 +90,7 @@ tnc_error tnc (char * host, char * port, char * user, unsigned int login) {
                 else if (len == 0) {
                     return TNCE_DISTANT_CLOSE;
                 }
+                // We are connected. Let's negociate about the window size
                 negotiate(sock, buf, 3);
             } else {
                 len = 1;
@@ -100,7 +102,7 @@ tnc_error tnc (char * host, char * port, char * user, unsigned int login) {
             buf[0] = getc(stdin); //fgets(buf, 1, stdin);
             if (send(sock, buf, 1, 0) < 0)
                 return TNCE_SUCCESS;
-            if (buf[0] == '\n') // with the terminal in raw mode we need to force a LF
+            if (buf[0] == '\n') 
                 putchar('\r');
         }
     }
